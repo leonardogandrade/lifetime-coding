0aa4eb57b06925015c3e20b952b8a84e936187fc
leonardogandrade <leonardo.g.andrade@gmail.com>
2023-10-27 13:42:18 -0300

    multi processing

--- python/advanced/multi-thread.py
+++ python/advanced/multi-thread.py
@@ -1,12 +1,36 @@
-from threading import Thread
+import os
+from threading import Thread, current_thread
+from multiprocessing import current_process
 import time
 
 
-def task():
-    time.sleep(2)
-    print("I have waited 2 seconds to process...")
+def task(id, stop_time):
+    # time.sleep(stop_time)
+    result = []
+
+    for i in range(1000):
+        for j in range(1000):
+            line = i * j
+            result.append(line)
+
+    print(
+        "PID: {}\tthread name: {}\tProcess Name: {}".format(
+            os.getpid(), current_thread().name, current_process().name
+        )
+    )
 
 
-thread_1 = Thread(target=task)
-thread_1.start()
-thread_1.join()
+queue = []
+
+for i in range(20):
+    thread = Thread(target=task, args=(1, 2))
+    thread.start()
+    queue.append(thread)
+
+start = time.time()
+
+for job in queue:
+    job.join()
+
+time_elapsed = time.time() - start
+print(f"Time elapsed: {time_elapsed:.2f}")
